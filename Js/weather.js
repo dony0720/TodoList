65d52ab5492a0e963e999edf033bf1c517f48fc0
dony0720 <97350693+dony0720@users.noreply.github.com>
2023-03-22 13:54:23 +0900

    Update weather.js

--- Js/weather.js
+++ Js/weather.js
@@ -6,9 +6,11 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
+    fetch(url) // 받은 요청 (response)을 then 메서드 콜백인자로 보낸다
+        .then((response) => response.json()) 
+        // response의 json() 메서드를 호출한다
+        // 응답내용을 json으로 변환 한 후 다음 then의 콜백인자로 넘겨준다
+        .then((data) => { // 넘겨받은 값을 통해 필요한 정보를 추출한다
             weather.innerText = `${data.name} / ${data.weather[0].main} / ${data.main.temp} `;
         });
 }
